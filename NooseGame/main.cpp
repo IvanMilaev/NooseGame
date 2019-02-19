7340009555a0df5db89f115e20cdd24a704452d3
Ivan Milaev <milaevivan@gmail.com>
2019-02-19 21:52:16 +0200

    Initial Commit

---
+++ NooseGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NooseGame
+//
+//  Created by Иван Милаев on 19/02/2019.
+//  Copyright © 2019 IvanMilaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
